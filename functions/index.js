4a545aa62fbb7677cf8daca0385575dd961be935
Adam Ortyl <adam.ortyl+github@gmail.com>
2018-03-26 12:42:41 +0000

    Getting the slack integration working

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const rp = require('request-promise');
+const REQUEST = require('request');
+
 admin.initializeApp(functions.config().firebase);
 
 
 exports.description = functions.https.onRequest((request, response) => {
-  response.set('Access-Control-Allow-Origin', "*")
-  response.set('Access-Control-Allow-Methods', 'GET, POST')
-  response.set('Access-Control-Allow-Headers', 'content-type')
+
     let p1 = admin.firestore().collection('adjectives').get()
       .then(snapshot => {
         if(snapshot.docs.length > 1) {
@@ -33,9 +33,17 @@ exports.description = functions.https.onRequest((request, response) => {
       });
 
     Promise.all([p1, p2]).then( results => {
-      return response.status(200).json(Object.assign({}, results[0], results[1]))
+      description = Object.assign({}, results[0], results[1]);
+
+      REQUEST.post(functions.config().slack.webhook_url,
+                   {json: { text: `Dan is a ${description.adjective} ${description.noun}`}});
+
+      response.set('Access-Control-Allow-Origin', "*")
+      response.set('Access-Control-Allow-Methods', 'GET, POST')
+      response.set('Access-Control-Allow-Headers', 'content-type')
+      response.status(200).json(description);
     }).catch((err) => {
-      return response.send(err);
+      response.send(err);
     });
 });
 
@@ -54,6 +62,11 @@ function postToSlack(msg) {
     uri: functions.config().slack.webhook_url,
     body: {
       text: `Dan is a ${msg}`,
+    },
+    headers: {
+        'User-Agent': 'Request-Promise',
+        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Methods': 'POST'
     },
     json: true,
   });
